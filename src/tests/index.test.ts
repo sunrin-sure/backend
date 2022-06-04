bc3a2f6316027643e5725b58985f88b1b1b89d03
lnJoy <lnjoy3306@gmail.com>
2022-06-05 03:47:33 +0900

    feat: 인덱스 테스트 추가

---
+++ src/tests/index.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+
+import App from '@/app';
+import IndexRoute from '@routes/index.route';
+
+describe('Testing Index', () => {
+  describe('[GET] /', () => {
+    it('response statusCode 200', () => {
+      const indexRoute = new IndexRoute();
+      const app = new App([indexRoute]);
+
+      return request(app.getServer()).get(`${indexRoute.path}`).expect(200);
+    });
+  });
+});
