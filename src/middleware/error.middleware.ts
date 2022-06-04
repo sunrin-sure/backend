f5c996fe3a254be9ab28f1c7226e342a7696e376
lnJoy <20sunrin007@sunrint.hs.kr>
2022-06-04 19:51:00 +0900

    feat: express 에러 핸들링 추가

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpException } from "@/exceptions/HttpException";
+
+import { logger } from "@/utils/logger";
+
+const errorMiddleware = (err: HttpException, req: Request, res: Response, next: NextFunction) => {
+  try {
+    const status: number = err.status || 500;
+    const message: string = err.message || 'Something went wrong';
+
+    logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message: ${message}`);
+    res.status(status).json({ message });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default errorMiddleware;
