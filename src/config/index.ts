a68f257972444f718a80399ae30c2abe38e37a6d
lnJoy <lnjoy3306@gmail.com>
2022-06-05 19:54:44 +0900

    fix: env 변수 변경

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,8 +5,7 @@ export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { 
   NODE_ENV, 
   PORT, 
-  DB_HOST, 
-  DB_PORT, 
+  DB_URL,  
   DB_DATABASE, 
   SECRET_KEY, 
   LOG_FORMAT, 
