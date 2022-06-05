3dc018c28495ed4122ca999978094a99735ec7f5
lnJoy <lnjoy3306@gmail.com>
2022-06-05 20:08:54 +0900

    fix: URL 변경
    * fix: +srv 형식의 URL 사용으로 변경
    * fix: options 에러 수정

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,10 @@
-import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+import { DB_URL, DB_DATABASE } from '@config';
 
 export const dbConnection = {
-  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  url: DB_URL,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
+    dbName: DB_DATABASE,
   },
 };
