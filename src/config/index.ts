72ae2ed07dc4ebfc8458a8382689da97271f99af
lnJoy <lnjoy3306@gmail.com>
2022-06-06 05:00:05 +0900

    feat: JWT 유효시간 추가
    * fix: typo

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,16 @@
 import { config } from 'dotenv';
+
 config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { 
-  NODE_ENV, 
-  PORT, 
-  DB_URL,  
-  DB_DATABASE, 
-  SECRET_KEY, 
-  LOG_FORMAT, 
-  LOG_DIR, 
-  ORIGIN } = process.env;
+export const {
+  NODE_ENV,
+  PORT,
+  DB_URL,
+  DB_DATABASE,
+  SECRET_KEY,
+  JWT_EXPIRATION,
+  LOG_FORMAT,
+  LOG_DIR,
+  ORIGIN,
+} = process.env;
