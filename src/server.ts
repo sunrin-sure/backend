35159da5b2043f62d5e63225341ad14ff8abfa8a
lnJoy <lnjoy3306@gmail.com>
2022-06-05 19:54:16 +0900

    feat: 유저 라우트 추가

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import App from '@/app';
+
 import IndexRoute from '@routes/index.route';
+import UsersRoute from '@routes/users.route';
 
 const app = new App([
-  new IndexRoute()
+  new IndexRoute(),
+  new UsersRoute(),
 ]);
 
 app.listen();
