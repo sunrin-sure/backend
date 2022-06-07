b73b00c684e7d7ad7db3785709b9aab601c429bc
lnJoy <lnjoy3306@gmail.com>
2022-06-07 11:02:27 +0900

    feat: 관리자일 경우 사용자 인증 없이 작동하도록 추가

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -4,6 +4,7 @@ import UserService from '@services/users.service';
 import { User } from '@interfaces/users.interface';
 import { UserDto } from '@dtos/users.dto';
 import { HttpException } from '@exceptions/HttpException';
+import { JwtUserPayload } from '@interfaces/jwt.interface';
 
 class UsersController {
   public userService = new UserService();
@@ -31,10 +32,15 @@ class UsersController {
   public updateUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userId: string = req.params.id;
-      const tokenUserId: string = req.jwtPayload.id;
+      const tokenPayload: JwtUserPayload = req.jwtPayload;
       const userData: UserDto = req.body;
-      if (userId !== tokenUserId) next(new HttpException(401, "Token is invalid"));
-      const updateUserData: User = await this.userService.updateUser(userId, userData);
+      let updateUserData: User;
+      if (tokenPayload.admin) {
+         updateUserData = await this.userService.updateUser(userId, userData);
+      } else {
+        if (userId !== tokenPayload.id) next(new HttpException(401, "Token is invalid"));
+        updateUserData = await this.userService.updateUser(userId, userData);
+      }
 
       res.status(200).json({ data: updateUserData, message: 'updated' });
     } catch (error) {
@@ -45,9 +51,14 @@ class UsersController {
   public deleteUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userId: string = req.params.id;
-      const isAdmin: boolean = req.jwtPayload.admin;
-      if (!isAdmin) next(new HttpException(401, "Not Admin")); 
-      const deleteUserData = await this.userService.deleteUser(userId);
+      const tokenPayload: JwtUserPayload = req.jwtPayload;
+      let deleteUserData: User;
+      if (tokenPayload.admin) {
+        deleteUserData = await this.userService.deleteUser(userId);
+      } else {
+        if (userId !== tokenPayload.id) next(new HttpException(401, "Token is invalid"));
+        deleteUserData = await this.userService.deleteUser(userId);
+      } 
 
       res.status(200).json({ data: deleteUserData, message: 'deleted' });
     } catch (error) {
