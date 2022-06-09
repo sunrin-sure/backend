b62cc481469da29b484de19bad2ca2f74c3c6d56
lnJoy <20sunrin007@sunrint.hs.kr>
2022-06-10 01:31:33 +0900

    feat: 토큰이 담길 쿠키의 만료시간을 계산해주는 모듈 추가

---
+++ src/utils/cookie.utils.ts
@@ -0,0 +1,24 @@
+const getCookieTime = (time: string, type: 'expires' | 'maxage') => {
+    const num = parseInt(time.replace(/[a-z]/gi, ''));
+    const unit = time.replace(/[0-9]/g, '');
+
+    let seconds: number;
+    
+    switch (unit) {
+        case 'd': seconds = num * 3600 * 24; break;
+        case 'h': seconds = num * 3600;      break;
+        case 'm': seconds = num * 60;        break;
+        case 's': seconds = num;             break;
+    };
+
+    let result;
+    if (type === 'expires') {
+        result = new Date(Date.now() + seconds * 1000);
+    } else if (type === 'maxage') {
+        result = seconds * 1000;
+    }
+
+    return result;
+}
+
+export default getCookieTime;
