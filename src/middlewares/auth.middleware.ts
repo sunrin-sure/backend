85036e4db53052f0feccc71d6419b4b1facc5a6d
lnJoy <20sunrin007@sunrint.hs.kr>
2022-06-10 01:26:09 +0900

    refactor: 에러 핸들링 수정

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,17 +2,15 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '@exceptions/HttpException';
 
 import { verify } from '@utils/jwt.utils';
+import { SECRET_KEY } from '@config';
 
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   if (req.headers.authorization) {
     const token = req.headers.authorization.split('Bearer ')[1];
-    const result = verify(token);
-    if (result.ok) {
-      req.jwtPayload = result.payload;
-      next();
-    } else {
-      next(new HttpException(401, result.message));
-    }
+    const result = verify(token, SECRET_KEY);
+    if (result instanceof HttpException) next(result);
+    else req.jwtPayload = result;
+    next();
   } else {
     next(new HttpException(401, 'Token is empty'));
   }
