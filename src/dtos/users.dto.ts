0b60f0d37648f191af865786a96064bcb6ee3a9b
lnJoy <20sunrin007@sunrint.hs.kr>
2022-06-12 23:59:42 +0900

    feat: usersDTO 정규식 추가
    * feat: 유저 닉네임과 비밀번호에 정규식을 추가하여 필터링하는 기능 추가

--- src/dtos/users.dto.ts
+++ src/dtos/users.dto.ts
@@ -1,15 +1,17 @@
 import {
-  IsArray, IsEmail, IsString,
+  IsArray, IsEmail, IsString, Matches,
 } from 'class-validator';
 
 export class UserDto {
   @IsString()
+  @Matches(/[a-zA-Z0-9_-]{2,12}/)
   public username: string;
 
   @IsEmail()
   public email: string;
 
   @IsString()
+  @Matches(/^\$2[ayb]\$[\d]{2}\$[./A-Za-z0-9]{53}$/)
   public password: string;
 
   @IsArray()
