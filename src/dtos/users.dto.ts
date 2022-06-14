fcf98b488eb7e742845c5f9351e5e227a6d9b685
lnJoy <lnjoy3306@gmail.com>
2022-06-14 19:23:50 +0900

    fix: 정규식 수정
    * feat: stacks 추가
    
    * fix: fields 타입 수정
    
    * fix: typo

--- src/dtos/users.dto.ts
+++ src/dtos/users.dto.ts
@@ -1,19 +1,32 @@
+import 'reflect-metadata';
+import { JobType } from '@types/users/user.job.type';
+import { Type } from 'class-transformer';
 import {
-  IsArray, IsEmail, IsString, Matches,
+  IsArray, IsEmail, IsString, Matches, ValidateNested,
 } from 'class-validator';
 
 export class UserDto {
   @IsString()
-  @Matches(/[a-zA-Z0-9_-]{2,12}/)
+  @Matches(
+    /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{1,10}/,
+    { message: 'username must match regular expression' },
+  )
   public username: string;
 
   @IsEmail()
   public email: string;
 
   @IsString()
-  @Matches(/^\$2[ayb]\$[\d]{2}\$[./A-Za-z0-9]{53}$/)
+  @Matches(
+    /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
+    { message: 'password must match regular expression' },
+  )
   public password: string;
 
+  @ValidateNested({ each: true })
+  @Type(() => JobType)
+  public fields: JobType[];
+
   @IsArray()
-  public fields: string[];
+  public stacks: string[];
 }
