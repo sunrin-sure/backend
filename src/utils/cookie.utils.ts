817f19b8b36e14c56d1992e7dbc3661aaa2d96ad
lnJoy <lnjoy3306@gmail.com>
2022-06-14 19:25:00 +0900

    fix: parseInt에서 Number로 변경

--- src/utils/cookie.utils.ts
+++ src/utils/cookie.utils.ts
@@ -1,24 +1,25 @@
 const getCookieTime = (time: string, type: 'expires' | 'maxage') => {
-    const num = parseInt(time.replace(/[a-z]/gi, ''));
-    const unit = time.replace(/[0-9]/g, '');
+  const num = Number(time.replace(/[a-z]/gi, ''));
+  const unit = time.replace(/[0-9]/g, '');
 
-    let seconds: number;
-    
-    switch (unit) {
-        case 'd': seconds = num * 3600 * 24; break;
-        case 'h': seconds = num * 3600;      break;
-        case 'm': seconds = num * 60;        break;
-        case 's': seconds = num;             break;
-    };
+  let seconds: number;
 
-    let result;
-    if (type === 'expires') {
-        result = new Date(Date.now() + (seconds * 1000));
-    } else if (type === 'maxage') {
-        result = seconds * 1000;
-    }
+  switch (unit) {
+    case 'd': seconds = num * 3600 * 24; break;
+    case 'h': seconds = num * 3600; break;
+    case 'm': seconds = num * 60; break;
+    case 's': seconds = num; break;
+    default: return 0;
+  }
 
-    return result;
-}
+  let result;
+  if (type === 'expires') {
+    result = new Date(Date.now() + (seconds * 1000));
+  } else if (type === 'maxage') {
+    result = seconds * 1000;
+  }
+
+  return result;
+};
 
 export default getCookieTime;
