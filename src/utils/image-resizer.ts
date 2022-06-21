d84abc19f77ee89ec6ab3b06e7aac6e0a605f257
lnJoy <lnjoy3306@gmail.com>
2022-06-21 22:43:53 +0900

    fix: resize 크기 추가
    * feat: png 추가 및 투명화 지원 추가

--- src/utils/image-resizer.ts
+++ src/utils/image-resizer.ts
@@ -1,6 +1,7 @@
 import sharp from 'sharp';
 
 export const avatarImageResizer = (file: Express.Multer.File): Promise<Buffer> => sharp(file.buffer)
-  .resize({ width: 640 })
+  .resize({ width: 256 })
   .withMetadata()
+  .png({ palette: true })
   .toBuffer();
