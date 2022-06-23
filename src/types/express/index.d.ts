9abb2254e65fdcb1db0f8fa60adfc481adb9a2f8
lnJoy <lnjoy3306@gmail.com>
2022-06-24 02:24:16 +0900

    fix: typo

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -2,6 +2,6 @@ import { JwtUserPayload } from '../../interfaces/jwt.interface';
 
 declare module 'express' {
   export interface Request {
-    jwtPayload: JwtUserPayload
+    jwtPayload: JwtUserPayload;
   }
 }
