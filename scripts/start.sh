7d6d929e5d07ac4dcc09addf7fdeccd65cc46d35
lnJoy <lnjoy3306@gmail.com>
2022-06-23 16:20:17 +0900

    feat: yarn으로 deploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-pm2 start ecosystem.config.js --only $ENV
+yarn deploy:$ENV
 
 CURRENT_PID=$(pm2 pid $ENV)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
