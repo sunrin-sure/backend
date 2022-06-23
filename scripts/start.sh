7bd6fa18ea1270ccac6113f47ead0c918a578309
lnJoy <lnjoy3306@gmail.com>
2022-06-23 11:37:58 +0900

    feat: AWS CodeDeploy 추가

---
+++ scripts/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="/home/ec2-user/app"
+
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+pm2 deploy:$ENV
+
+CURRENT_PID=$(pm2 pid $ENV)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
