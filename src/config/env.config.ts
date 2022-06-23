b5a185bf7545dbed8dcbf6cb5f15acc0b55bd21f
lnJoy <lnjoy3306@gmail.com>
2022-06-23 23:05:27 +0900

    fix: 헤로쿠에서 사용될 env 형식으로 변경

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,9 +1,26 @@
 import { config } from 'dotenv';
 
-config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+config();
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const {
+
+const NODE_ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+const DB_DATABASE = process.env.DB_DATABASE;
+const CLOUD_NAME = process.env.CLOUD_NAME;
+const CLOUD_API_KEY = process.env.CLOUD_API_KEY;
+const CLOUD_API_SECRET = process.env.CLOUD_API_SECRET;
+const DEFAULT_AVATAR = process.env.DEFAULT_AVATAR;
+const SECRET_KEY = process.env.SECRET_KEY;
+const R_SECRET_KEY = process.env.R_SECRET_KEY;
+const ACCESS_EXPIRATION = process.env.ACCESS_EXPIRATION;
+const REFRESH_EXPIRATION = process.env.REFRESH_EXPIRATION;
+const LOG_FORMAT = process.env.LOG_FORMAT;
+const LOG_DIR = process.env.LOG_DIR;
+const ORIGIN = process.env.ORIGIN;
+
+export {
   NODE_ENV,
   PORT,
   DB_URL,
@@ -19,4 +36,4 @@ export const {
   LOG_FORMAT,
   LOG_DIR,
   ORIGIN,
-} = process.env;
+};
