f9145d5152a322d9e9b218822c4c017bdbe3d17c
lnJoy <lnjoy3306@gmail.com>
2022-06-25 01:41:00 +0900

    fix: 모집글 수정 또는 삭제를 할 때 유저 인증 기능 추가
    * feat: 모집글 생성 기능 추가

--- src/services/project-post.service.ts
+++ src/services/project-post.service.ts
@@ -8,40 +8,60 @@ class ProjectPostService {
   public posts = projectPostModel;
 
   public async findAllProjectPost(): Promise<ProjectPost[]> {
-    const posts: ProjectPost[] = await this.posts.find();
+    const posts: ProjectPost[] = await this.posts.find().select('-contents');
     return posts;
   }
 
   public async findProjectPostById(postId: string): Promise<ProjectPost> {
-    if (isEmpty(postId)) throw new HttpException(400, "You're not postId");
+    if (isEmpty(postId)) throw new HttpException(400, 'You\'re not postId');
 
     const findProjectPost: ProjectPost = await this.posts.findOne({
       _id: postId,
     });
-    if (!findProjectPost) throw new HttpException(409, "You're not user");
+    if (!findProjectPost) throw new HttpException(409, 'You\'re not post');
 
     return findProjectPost;
+  }
+
+  public async createProjectPost(userId: string, projectPostData: ProjectPostDto): Promise<ProjectPost> {
+    if (isEmpty(projectPostData)) throw new HttpException(400, 'You\'re not postData');
+
+    const newProjectPost: ProjectPost = await this.posts.create({
+      ...projectPostData,
+      author: userId,
+    });
+
+    return newProjectPost;
   }
 
   public async updateProjectPost(
     postId: string,
     postData: ProjectPostDto,
+    userId?: string,
   ): Promise<ProjectPost> {
-    if (isEmpty(postId)) throw new HttpException(400, "You're not postId");
+    if (isEmpty(postId)) throw new HttpException(400, 'You\'re not postId');
 
+    if (!userId) {
+      const projectPostAuthorId = (await this.findProjectPostById(postId)).author;
+      if (userId === projectPostAuthorId) throw new HttpException(401, 'You don\'t have permission');
+    }
     const updateProjectPostById: ProjectPost = await this.posts.findByIdAndUpdate(
       postId,
       { $set: postData },
       { returnDocument: 'after' },
     );
-    if (!updateProjectPostById) throw new HttpException(409, "You're not post");
+    if (!updateProjectPostById) throw new HttpException(409, 'You\'re not post');
 
     return updateProjectPostById;
   }
 
-  public async deleteProjectPost(userId: string): Promise<ProjectPost> {
-    const deleteProjectPostById: ProjectPost = await this.posts.findByIdAndDelete(userId);
-    if (!deleteProjectPostById) throw new HttpException(409, "You're not post");
+  public async deleteProjectPost(postId: string, userId?: string): Promise<ProjectPost> {
+    if (!userId) {
+      const projectPostAuthorId = (await this.findProjectPostById(postId)).author;
+      if (userId === projectPostAuthorId) throw new HttpException(401, 'You don\'t have permission');
+    }
+    const deleteProjectPostById: ProjectPost = await this.posts.findByIdAndDelete(postId);
+    if (!deleteProjectPostById) throw new HttpException(409, 'You\'re not post');
 
     return deleteProjectPostById;
   }
